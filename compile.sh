1cbd613f33494cd9e2d3c68e6793bfdbc4d74268
Justin Hempsey <flinestone@rocketmail.com>
2016-05-24 17:43:56 -0700

    sql connection testing

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 rm -rf webapps/ROOT/WEB-INF/classes/*.class
 javac -cp lib/servlet-api.jar webapps/ROOT/WEB-INF/classes/*
+$CATALINA_HOME/bin/shutdown.sh > /dev/null
+$CATALINA_HOME/bin/startup.sh > /dev/null
--- webapps/ROOT/WEB-INF/classes/HomeServlet.java
+++ webapps/ROOT/WEB-INF/classes/HomeServlet.java
@@ -1,9 +1,16 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 //@WebServlet("/helloworld")
+//Class.forName ("com.mysql.jdbc.Driver");
 public class HomeServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection con = null;
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter())
         {
@@ -13,10 +20,18 @@ public class HomeServlet extends HttpServlet{
             out.println("<title>Servlet helloworld</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println ("<h1>Hello World</h1><br/>");
+            out.println ("<h1>Hello World!!</h1><br/>");
             out.println("<h1>Servlet helloworld at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
-        }  // end doGet()
+            String servername = "sylvester-mccoy-v3.ics.uci.edu";
+            String db="inf124grp13";
+            String username = "inf124grp13";
+            String password = "4a=eDuVu";
+
+            con = DriverManager.getConnection("jdbc:mysql://" + servername + "/" + db, username, password);
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        } // end doGet()
     }  // end helloworld
 }
