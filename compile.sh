34335ce9a1e397a2e9aaa0d4748ec66e91f9fc94
Justin Hempsey <flinestone@rocketmail.com>
2016-05-25 16:29:47 -0700

    connects to data base

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 rm -rf webapps/ROOT/WEB-INF/classes/*.class
 javac -cp lib/servlet-api.jar webapps/ROOT/WEB-INF/classes/*
-$CATALINA_HOME/bin/shutdown.sh > /dev/null
-$CATALINA_HOME/bin/startup.sh > /dev/null
+if [[ "$?" -eq 0 ]]; then 
+	$CATALINA_HOME/bin/shutdown.sh > /dev/null
+	$CATALINA_HOME/bin/startup.sh > /dev/null
+fi
--- webapps/ROOT/WEB-INF/classes/HomeServlet.java
+++ webapps/ROOT/WEB-INF/classes/HomeServlet.java
@@ -11,9 +11,13 @@ import java.sql.Statement;
 public class HomeServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter())
         {
+            Class.forName("com.mysql.jdbc.Driver");
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -21,17 +25,28 @@ public class HomeServlet extends HttpServlet{
             out.println("</head>");
             out.println("<body>");
             out.println ("<h1>Hello World!!</h1><br/>");
-            out.println("<h1>Servlet helloworld at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            //out.println("<h1>Servlet helloworld at " + request.getContextPath() + "</h1>");
+
             String servername = "sylvester-mccoy-v3.ics.uci.edu";
             String db="inf124grp13";
             String username = "inf124grp13";
             String password = "4a=eDuVu";
 
             con = DriverManager.getConnection("jdbc:mysql://" + servername + "/" + db, username, password);
+            stmt = con.createStatement();
+
+            rs = stmt.executeQuery("SELECT * FROM peppers");
+            out.println("<p>HI</p>");
+            while (rs.next()){
+                out.println("<p>" + rs.getString("pepper_name")+ "</p>");
+            }
+
+            out.println("</body>");
+            out.println("</html>");
         } catch (SQLException ex){
             ex.printStackTrace();
-        } // end doGet()
-    }  // end helloworld
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }  // end doGet(
 }
