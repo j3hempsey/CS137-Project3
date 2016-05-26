08d75afa4f814434c1ae30aae5e847664f58114d
Justin Hempsey <flinestone@rocketmail.com>
2016-05-25 19:50:21 -0700

    working homepage except for links

--- webapps/ROOT/WEB-INF/classes/HomeServlet.java
+++ webapps/ROOT/WEB-INF/classes/HomeServlet.java
@@ -6,13 +6,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-//@WebServlet("/helloworld")
-//Class.forName ("com.mysql.jdbc.Driver");
+import java.util.ArrayList;
+
 public class HomeServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
+        ArrayList<ArrayList<String>> pepperList = new ArrayList<ArrayList<String>>();
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter())
@@ -24,8 +25,7 @@ public class HomeServlet extends HttpServlet{
             out.println("<title>Servlet helloworld</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println ("<h1>Hello World!!</h1><br/>");
-            //out.println("<h1>Servlet helloworld at " + request.getContextPath() + "</h1>");
+            out.println("<p>HI</p>");
 
             String servername = "sylvester-mccoy-v3.ics.uci.edu";
             String db="inf124grp13";
@@ -36,13 +36,19 @@ public class HomeServlet extends HttpServlet{
             stmt = con.createStatement();
 
             rs = stmt.executeQuery("SELECT * FROM peppers");
-            out.println("<p>HI</p>");
             while (rs.next()){
-                out.println("<p>" + rs.getString("pepper_name")+ "</p>");
+                ArrayList<String> pepper = new ArrayList<String>();
+                pepper.add(rs.getString("url"));
+                pepper.add(rs.getString("image_url"));
+                pepper.add(rs.getString("pepper_name"));
+                pepper.add(rs.getString("pepper_type"));
+                pepper.add(rs.getString("spicy_creative"));
+                pepper.add(rs.getString("price"));
+                pepperList.add(pepper);
             }
+            request.setAttribute("peppers", pepperList);
 
-            out.println("</body>");
-            out.println("</html>");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
         } catch (SQLException ex){
             ex.printStackTrace();
         } catch (Exception e){
