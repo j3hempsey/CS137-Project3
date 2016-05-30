6a796e333a52c739198d039c1f10c7fe10432e44
Jed <jedgentry@mac.com>
2016-05-30 12:43:40 -0700

    Compile Script handles duplicate files.

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 rm -rf webapps/ROOT/WEB-INF/classes/*.class
 
-
-#javac -cp lib/servlet-api.jar webapps/ROOT/WEB-INF/classes/Models/*.java
-javac -cp "lib/servlet-api.jar:lib/gson-2.2.2.jar" -sourcepath "webapps/ROOT/WEB-INF/classes/" -Xlint:unchecked webapps/ROOT/WEB-INF/classes/*.java
-
+#Handle duplicate file names (Needed for my listener)
+FILES=`find ${CATALINA_HOME}/webapps/ROOT/WEB-INF/classes -type f -name "*.java"`
+for var in $FILES
+do
+    javac -cp "lib/servlet-api.jar:lib/gson-2.2.2.jar" -sourcepath "webapps/ROOT/WEB-INF/classes/" -Xlint:unchecked $var
+done
 
 if [[ "$?" -eq 0 ]]; then 
 	$CATALINA_HOME/bin/shutdown.sh > /dev/null
