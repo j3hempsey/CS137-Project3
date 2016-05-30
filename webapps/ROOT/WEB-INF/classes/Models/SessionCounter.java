73a2df127c402e2babcc63fd55eff650f996c0b6
Jed <jedgentry@mac.com>
2016-05-29 18:53:06 -0700

    Not needed anymore.

--- webapps/ROOT/WEB-INF/classes/Models/SessionCounter.java
+++ webapps/ROOT/WEB-INF/classes/Models/SessionCounter.java
@@ -8,14 +8,12 @@ public class SessionCounter implements HttpSessionListener {
     private List<String> sessions = new ArrayList<>();
 
     public void sessionCreated(HttpSessionEvent event) {
-        System.out.println("SessionCounter.sessionCreated");
         HttpSession session = event.getSession();
         sessions.add(session.getId());
         session.setAttribute("session-counter", this);
     }
 
     public void sessionDestroyed(HttpSessionEvent event) {
-        System.out.println("SessionCounter.sessionDestroyed");
         HttpSession session = event.getSession();
         sessions.remove(session.getId());
         session.setAttribute("session-counter", this);
